f59c63cbe191019722f0dff477498860d469f5ed
Souvik <souvikbiswas792@gmail.com>
2021-09-05 17:01:49 +0530

    Linked api server

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 let txtInput = document.querySelector("#txt-input");
 let btnTranslate = document.querySelector("#btn-translate");
 let txtOutput = document.querySelector("#output");
+
+let serverURL = "https://api.funtranslations.com/translate/yoda.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "texr=" + text;
+}
